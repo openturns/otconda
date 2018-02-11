0f8c9efe7d9719f81d7e8d5fef06b7fbfea1e384
Julien Schueller <schueller@phimeca.com>
2018-02-11 19:11:04 +0100

    Initial import

---
+++ README.rst
@@ -0,0 +1,11 @@
+.. image:: https://travis-ci.org/openturns/otconda.svg
+    :target: https://travis-ci.org/openturns/otconda
+
+.. image:: https://ci.appveyor.com/api/projects/status/w3ao2a3g2u1m3pv9?svg=true
+    :target: https://ci.appveyor.com/project/openturns/otconda
+
+OTconda installer
+=================
+
+Conda bundle including OpenTURNS ands its modules
+
---
+++ otconda_test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import re
+import importlib
+import sys
+import traceback
+
+
+def parse_modules(filename):
+    modules = []
+    with open(filename) as construct:
+        start = False
+        end = False
+        for line in construct.readlines():
+            if start:
+                m = re.search('^  - ([\w]+)$', line)
+                if m is not None:
+                    modules.append(m.group(1))
+                else:
+                    m = re.search('license_file:', line)
+                    if m is not None:
+                        end = True
+            else:
+                m = re.search('specs:', line)
+                if m is not None:
+                    start = True
+            if end:
+                break
+    return modules
+
+
+
+def check_modules(modules):
+    n_fail = 0
+    for mod in modules:
+        try:
+            importlib.import_module(mod)
+            print(mod.ljust(40), 'OK')
+        except ImportError as exc:
+            n_fail += 1
+            print(mod.ljust(40), '***Failed')
+            traceback.print_exc()
+    return n_fail
+
+
+if __name__ == '__main__':
+    modules = parse_modules('construct.yaml')
+    n_fail = check_modules(modules)
+    sys.exit(n_fail)
