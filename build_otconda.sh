bc6c048d1feb9e4b8f0811dd01d8344e38490f8d
Julien Schueller <schueller@phimeca.com>
2018-04-15 11:26:28 +0200

    Use build_otconda.sh

--- build_otconda.sh
+++ build_otconda.sh
@@ -2,18 +2,30 @@
 
 set -e
 
-PY_MAJOR_VER=3
+usage()
+{
+  echo "Usage: $0 PY_MAJOR_VER"
+  exit 1
+}
 
+test $# = 1 || usage
+
+PY_MAJOR_VER=$1
+
+# install constructor
 wget -c --no-check-certificate https://repo.continuum.io/miniconda/Miniconda${PY_MAJOR_VER}-latest-Linux-x86_64.sh -P /tmp
-rm -rf $HOME/miniconda $HOME/.condarc
-bash /tmp/Miniconda${PY_MAJOR_VER}-latest-Linux-x86_64.sh -b -p $HOME/miniconda
-PATH="$HOME/miniconda/bin:$PATH"
+rm -rf /tmp/miniconda
+bash /tmp/Miniconda${PY_MAJOR_VER}-latest-Linux-x86_64.sh -b -p /tmp/miniconda
+PATH="/tmp/miniconda/bin:$PATH"
 conda install -y constructor
+conda install -y conda=4.3  # pin conda to fix pyqt resolve
 
+# build
 sed "s|@PY_MAJOR_VER@|${PY_MAJOR_VER}|g" construct.yaml.in > construct.yaml
 constructor .
 
-rm -rf $HOME/otconda
-bash otconda${PY_MAJOR_VER}*.sh -b -p $HOME/otconda
-PATH="$HOME/otconda/bin:$PATH"
+# test
+rm -rf /tmp/otconda
+bash otconda${PY_MAJOR_VER}*.sh -b -p /tmp/otconda
+PATH="/tmp/otconda/bin:$PATH"
 python otconda_test.py
