9ef0d2207c48117afd11f6453667ae96d53e076c
Julien Schueller <schueller@phimeca.com>
2019-01-25 10:06:56 +0100

    constructor/issues/204

--- build_otconda.sh
+++ build_otconda.sh
@@ -26,9 +26,6 @@ bash /tmp/Miniconda${PY_MAJOR_VER}-latest-${OS}-x86_64.sh -b -p /tmp/miniconda
 PATH="/tmp/miniconda/bin:$PATH"
 conda install -y constructor
 
-# https://github.com/conda/constructor/issues/204
-#conda install -y conda=4.3
-
 # build
 rm -f otconda${PY_MAJOR_VER}*.sh
 sed "s|@PY_MAJOR_VER@|${PY_MAJOR_VER}|g" construct.yaml.in > construct.yaml
